2b7b715cfeaa407adbc4bf29af3dd80dcd8e5a85
Tomas Rutkauskas <tomas@baltsoft.com>
2023-11-30 15:01:21 +0200

    Update ConvertHtmlToPdf project to .NET 6.0

--- ConvertAPI.Example.DotNet/ConvertHtmlToPdf/Program.cs
+++ ConvertAPI.Example.DotNet/ConvertHtmlToPdf/Program.cs
@@ -1,47 +1,45 @@
-﻿using System;
-using System.IO;
-using System.Net;
-using System.Text;
-using System.Net.Http;
-using System.Threading.Tasks;
+﻿using System.Text;
 
 namespace ConvertHtmlToPdf
 {
     internal class Program
     {
-        public static async Task Main (string[] args) {
+        public static async Task Main(string[] args)
+        {
             const string secret = "";
-            var fileToSave = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "file.pdf");           
+            var outputFile = Path.Combine(Directory.GetCurrentDirectory(), "output.pdf");
 
-            if(string.IsNullOrEmpty(secret))
-                Console.WriteLine("The secret is missing, get one for free at https://www.convertapi.com/a");
+            if (string.IsNullOrEmpty(secret))
+                Console.WriteLine("The secret is missing, get one for free at https://www.convertapi.com/a/auth");
             else
             {
                 try
                 {
                     Console.WriteLine("Please wait, converting!");
-                    var url = new Uri($"https://v2.convertapi.com/html/to/pdf?download=attachment&secret={secret}");
-                    var htmlString = "<!doctype html><html lang=en><head><meta charset=utf-8><title>ConvertAPI test</title></head><body>This page is generated from HTML string.</body></html>";
+                    const string htmlString = "<!doctype html><html lang=en><head><meta charset=utf-8><title>ConvertAPI test</title></head><body>This page is generated from HTML string.</body></html>";
                     var content = new StringContent(htmlString, Encoding.UTF8, "application/octet-stream");
                     content.Headers.Add("Content-Disposition", "attachment; filename=\"data.html\"");
-                    using (var resultFile = File.OpenWrite(fileToSave))
+
+                    using (var httpClient = new HttpClient())
+                    using (var resultFile = File.OpenWrite(outputFile))
                     {
-                        var request = new HttpClient().PostAsync(url, content);
-                        if (request.Result.IsSuccessStatusCode)
+                        var response = await httpClient.PostAsync($"https://v2.convertapi.com/html/to/pdf?download=attachment&secret={secret}", content);
+
+                        if (response.IsSuccessStatusCode)
                         {
-                            request.Result.Content.CopyToAsync(resultFile).Wait();
-                            Console.WriteLine("File converted successfully");
+                            await response.Content.CopyToAsync(resultFile);
+                            Console.WriteLine("File converted successfully " + outputFile);
                         }
                         else
                         {
-                            Console.WriteLine("Status Code : {0}", request.Result.StatusCode);
-                            Console.WriteLine("Body : {0}", await request.Result.Content.ReadAsStringAsync());
+                            Console.WriteLine($"Status Code : {response.StatusCode}");
+                            Console.WriteLine($"Body : {await response.Content.ReadAsStringAsync()}");
                         }
                     }
                 }
-                catch (WebException e)
+                catch (HttpRequestException e)
                 {
-
+                    Console.WriteLine("An error occurred: {0}", e.Message);
                 }
             }
         }
