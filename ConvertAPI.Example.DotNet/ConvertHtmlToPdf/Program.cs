15912e9bd69255f516b62cf1199d0e728c7c1cdd
Tomas Rutkauskas <tomas@baltsoft.com>
2023-11-30 15:17:51 +0200

    Simplify code structure by removing unnecessary namespace and class declarations
    The namespace and class declarations previously wrapping the code in Program.cs served no functional purpose and increased the complexity of the code structure. This change removes those declarations, making the code easier to read and maintain. This commit does not affect the behavior or output of the code.

--- ConvertAPI.Example.DotNet/ConvertHtmlToPdf/Program.cs
+++ ConvertAPI.Example.DotNet/ConvertHtmlToPdf/Program.cs
@@ -1,47 +1,38 @@
 ﻿using System.Text;
 
-namespace ConvertHtmlToPdf
-{
-    internal class Program
-    {
-        public static async Task Main(string[] args)
-        {
-            const string secret = "";
-            var outputFile = Path.Combine(Directory.GetCurrentDirectory(), "output.pdf");
+const string secret = "";
+var outputFile = Path.Combine(Directory.GetCurrentDirectory(), "output.pdf");
 
-            if (string.IsNullOrEmpty(secret))
-                Console.WriteLine("The secret is missing, get one for free at https://www.convertapi.com/a/auth");
+if (string.IsNullOrEmpty(secret))
+    Console.WriteLine("The secret is missing, get one for free at https://www.convertapi.com/a/auth");
+else
+{
+    try
+    {
+        Console.WriteLine("Please wait, converting!");
+        const string htmlString = "<!doctype html><html lang=en><head><meta charset=utf-8><title>ConvertAPI test</title></head><body>This page is generated from HTML string.</body></html>";
+        var content = new StringContent(htmlString, Encoding.UTF8, "application/octet-stream");
+        content.Headers.Add("Content-Disposition", "attachment; filename=\"data.html\"");
+
+        using (var httpClient = new HttpClient())
+        using (var resultFile = File.OpenWrite(outputFile))
+        {
+            var response = await httpClient.PostAsync($"https://v2.convertapi.com/convert/html/to/pdf?secret={secret}", content);
+
+            if (response.IsSuccessStatusCode)
+            {
+                await response.Content.CopyToAsync(resultFile);
+                Console.WriteLine("File converted successfully " + outputFile);
+            }
             else
             {
-                try
-                {
-                    Console.WriteLine("Please wait, converting!");
-                    const string htmlString = "<!doctype html><html lang=en><head><meta charset=utf-8><title>ConvertAPI test</title></head><body>This page is generated from HTML string.</body></html>";
-                    var content = new StringContent(htmlString, Encoding.UTF8, "application/octet-stream");
-                    content.Headers.Add("Content-Disposition", "attachment; filename=\"data.html\"");
-
-                    using (var httpClient = new HttpClient())
-                    using (var resultFile = File.OpenWrite(outputFile))
-                    {
-                        var response = await httpClient.PostAsync($"https://v2.convertapi.com/html/to/pdf?download=attachment&secret={secret}", content);
-
-                        if (response.IsSuccessStatusCode)
-                        {
-                            await response.Content.CopyToAsync(resultFile);
-                            Console.WriteLine("File converted successfully " + outputFile);
-                        }
-                        else
-                        {
-                            Console.WriteLine($"Status Code : {response.StatusCode}");
-                            Console.WriteLine($"Body : {await response.Content.ReadAsStringAsync()}");
-                        }
-                    }
-                }
-                catch (HttpRequestException e)
-                {
-                    Console.WriteLine("An error occurred: {0}", e.Message);
-                }
+                Console.WriteLine($"Status Code : {response.StatusCode}");
+                Console.WriteLine($"Body : {await response.Content.ReadAsStringAsync()}");
             }
         }
+    }
+    catch (HttpRequestException e)
+    {
+        Console.WriteLine("An error occurred: {0}", e.Message);
     }
 }
