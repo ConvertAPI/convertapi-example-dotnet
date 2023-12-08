41ef194f63b467ba890d6bbf7999a49a658db2f1
Tomas Rutkauskas <tomas@baltsoft.com>
2023-12-08 15:39:43 +0200

    Add file storage feature in ConvertAPI usage
    In this commit, we introduced an option for users to store the converted file to ConvertAPI's file storage. A new boolean constant 'StoreFile' has been added. If this constant is set to true, the file will be stored for maximum 3 hours in ConvertAPI's storage. We modified the FileConverter's SubmitFileForConversion method to accept an additional argument 'storeFile'. Accordingly, ConvertAPI's API request has been updated to include 'StoreFile' in query if 'storeFile' equals true. We also handled the API response to read JSON if the content type is 'application/json' and show the stored file's URL. Three new classes 'ConvertApiResponse', 'ConvertApiFiles' and 'AsyncResponse' have been added to deserialize JSON response.

--- ConvertAPI.Example.DotNet/AsyncConversion/Program.cs
+++ ConvertAPI.Example.DotNet/AsyncConversion/Program.cs
@@ -1,14 +1,20 @@
 ﻿using System.Net;
 using System.Net.Http.Headers;
+using System.Text.Json;
 using System.Web;
 
+//The secret should be set before calling API. You can obtain the secret from this ConvertAPI authentication page https://www.convertapi.com/a/auth
 const string secret = "";
+//You should set this property to True if you want the converted file to be saved to ConvertAPI's file storage.
+//Please note that the file will only be stored for a maximum of 3 hours, after which it will be automatically deleted.
+//However, you have the option to manually delete the file using HTTP Delete prior to the automatic deletion.
+const bool storeFile = true;
 
 const string inputFile = "SampleDOCFile_1000kb.docx";
 var outputFile = Path.Combine(Path.GetTempPath(), "output.pdf");
 
 var fileConverter = new FileConverter(new HttpClient(), secret);
-var jobId = await fileConverter.SubmitFileForConversion(inputFile);
+var jobId = await fileConverter.SubmitFileForConversion(inputFile, storeFile);
 
 Console.WriteLine($"The Word document has been sent for conversion. The task is identified by the Async ID: {jobId}. Now, we are awaiting the result.");
 
@@ -19,7 +25,7 @@ while (httpStatusCode == HttpStatusCode.Accepted)
     httpStatusCode = await fileConverter.WaitForConversion(jobId, outputFile);
     if (httpStatusCode == HttpStatusCode.Accepted)
     {
-        await Task.Delay(5000);   
+        await Task.Delay(5000);
     }
 }
 
@@ -38,7 +44,7 @@ public class FileConverter
         _client = client;
     }
 
-    public async Task<string> SubmitFileForConversion(string inputFile)
+    public async Task<string> SubmitFileForConversion(string inputFile, bool storeFile)
     {
         if (string.IsNullOrEmpty(_secret))
             throw new ArgumentException("The secret is missing, get one for free at https://www.convertapi.com/a/auth", nameof(_secret));
@@ -47,6 +53,12 @@ public class FileConverter
         var convertBuilder = new UriBuilder("https://v2.convertapi.com/async/convert/docx/to/pdf");
         var convertQuery = HttpUtility.ParseQueryString(convertBuilder.Query);
         convertQuery["Secret"] = _secret;
+        if (storeFile)
+        {
+            convertQuery["StoreFile"] = true.ToString();
+            _client.DefaultRequestHeaders.Add("accept", "application/json");
+        }
+
         convertBuilder.Query = convertQuery.ToString();
 
         var streamContent = new StreamContent(fs);
@@ -72,9 +84,16 @@ public class FileConverter
         switch (resultResponse.StatusCode)
         {
             case HttpStatusCode.OK:
-                await SaveConvertedFile(resultResponse, outputFile);
-                break;
+            {
+                if (string.Equals(resultResponse.Content.Headers.ContentType.MediaType, "application/json", StringComparison.OrdinalIgnoreCase))
+                {
+                    await ReadResponseJson(resultResponse);
+                }
+                else
+                    await SaveConvertedFile(resultResponse, outputFile);
 
+                break;
+            }
             case HttpStatusCode.Accepted:
                 Console.WriteLine("File conversion is in progress...");
                 break;
@@ -97,9 +116,34 @@ public class FileConverter
         await resultFile.CopyToAsync(outputFileStream);
         Console.WriteLine($"File conversion completed. The file saved to {outputFile}");
     }
+    
+    private async Task ReadResponseJson(HttpResponseMessage response)
+    {
+        var responseBody = await response.Content.ReadAsStringAsync();
+        var convertApiResponse = JsonSerializer.Deserialize<ConvertApiResponse>(responseBody);
+        foreach (var convertApiFiles in convertApiResponse.Files)
+        {
+            Console.WriteLine($"File conversion completed. The file stored at {convertApiFiles.Url}");   
+        }
+    }
 }
 
 public class AsyncResponse
 {
     public string JobId { get; set; }
+}
+
+public class ConvertApiResponse
+{
+    public int ConversionCost { get; set; }
+    public ConvertApiFiles[] Files { get; set; }
+}
+
+public class ConvertApiFiles
+{
+    public string FileId { get; set; }        
+    public string FileName { get; set; }        
+    public string FileExt { get; set; }
+    public int FileSize { get; set; }
+    public Uri Url { get; set; }
 }
